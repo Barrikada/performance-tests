caedb06d11dccd70db041655123a432abce28423
Barrikada <barrikadaa23@gmail.com>
2026-04-06 12:46:05 +0400

    Знакомство с Pydantic. Практика написания Pydantic-моделей

---
+++ pydantic_create_user.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel, EmailStr, Field
+
+
+class UserSchema(BaseModel):
+    """
+    Модель данных пользователя.
+    """
+
+    id: str
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+    phone_number: str = Field(alias="phoneNumber")
+
+
+class CreateUserRequestSchema(BaseModel):
+    """
+    Модель запроса на создание пользователя.
+    """
+
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+    phone_number: str = Field(alias="phoneNumber")
+
+
+class CreateUserResponseSchema(BaseModel):
+    """
+    Модель ответа на создание пользователя.
+    """
+
+    user: UserSchema
