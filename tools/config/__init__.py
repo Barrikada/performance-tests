1061c2607f711478c24df3c158e941dfc0248926
Barrikada <barrikadaa23@gmail.com>
2026-04-23 17:53:54 +0400

    Настройки нагрузочных тестов с Pydantic

---
+++ tools/config/__init__.py
@@ -0,0 +1,5 @@
+from tools.config.grpc import GRPCClientConfig
+from tools.config.http import HTTPClientConfig
+from tools.config.locust import LocustUserConfig
+
+__all__ = ["HTTPClientConfig", "GRPCClientConfig", "LocustUserConfig"]
